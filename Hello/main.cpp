8cf40a462f686cb420c6a6343d8c7f8d41a4695c
saurabhdhingra <saurabh20work@gmail.com>
2025-01-15 10:48:08 +0530

    Initial Commit

---
+++ Hello/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Hello
+//
+//  Created by Saurabh Dhingra on 15/01/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
