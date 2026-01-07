a962dfe80cd60cd0d37fe1dc70186b350ee78d19
saurabhdhingra <saurabh20work@gmail.com>
2026-01-07 14:09:15 +0530

    Hidden Integer

---
+++ Interactive_Problems/01_hidden_integer.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#include <iostream>
+
+using namespace std;
+const int maxX = 1e9;
+
+int main(){
+    string result;
+    int l = 0, r = maxX + 1;
+    while(l + 1 < r){
+        int m = l + (r-l) / 2;
+
+        cout << "? " << m << endl;
+        cin >> result;
+
+        if(result == "YES") l = m;
+        else                r = m;
+    }
+
+    cout << "! " << l+1 << endl;
+}
