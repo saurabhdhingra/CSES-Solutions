ed4231f430a3777f5c728deea31ef5bc61939ca4
saurabhdhingra <saurabh20work@gmail.com>
2026-03-30 23:25:26 +0530

    Minimizing Coins

---
+++ Dynamic Programming/02_minimizing_coins.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n, x;
+    if (!(cin >> n >> x)) return 0;
+
+    vector<int> coins(n);
+    for (int i = 0; i < n; i++) {
+        cin >> coins[i];
+    }
+
+    const int INF = 1e9;
+    vector<int> dp(x + 1, INF);
+
+    dp[0] = 0;
+
+    for (int i = 1; i <= x; i++) {
+        for (int c : coins) {
+            if (i - c >= 0) {
+                dp[i] = min(dp[i], dp[i - c] + 1);
+            }
+        }
+    }
+
+    if (dp[x] == INF) {
+        cout << -1 << "\n";
+    } else {
+        cout << dp[x] << "\n";
+    }
+
+    return 0;
+}
